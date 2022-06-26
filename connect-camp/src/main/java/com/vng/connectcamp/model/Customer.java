41b3d00e959ec53fb0d92f136b1108e78413390f
Vitor N. Guandalini <guandalinivitor@MacBook-Pro-de-Vitor.local>
2022-06-26 19:33:08 -0300

    Pushing files to repo service

---
+++ connect-camp/src/main/java/com/vng/connectcamp/model/Customer.java
@@ -0,0 +1,19 @@
+package com.vng.connectcamp.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String nome;
+}
---
+++ connect-camp/src/test/java/com/vng/connectcamp/ConnectCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vng.connectcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectCampApplicationTests {
+
+//	@Test
+//	//void contextLoads() {
+//	}
+
+}
