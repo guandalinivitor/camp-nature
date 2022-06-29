49c3039401a9cc32bec0053cd94246d493f68f08
guandalinivitor <guandalinivitor@gmail.com>
2022-06-28 21:38:45 -0300

    Fixing the repo

--- src/main/java/com/vng/connectcamp/model/RootEntryPointModel.java
+++
@@ -1,7 +0,0 @@
-package com.vng.connectcamp.model;
-
-import org.springframework.hateoas.RepresentationModel;
-
-public class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {
-
-}
